f8d4b6682feeaaf4990504db684d754205df9e1d
Cristian Ramirez <cristianramirezgt@gmail.com>
2022-05-13 12:51:04 -0600

    20220513 125104

--- src/router/router.js
+++ src/router/router.js
@@ -129,16 +129,37 @@ const router = createRouter({
 });
 
 // Guard global - syncronic
-router.beforeEach((to, from, next) => {
-  console.log({ to, from, next });
-  const random = Math.random() * 100
-  if ( random > 50 ) {
-    console.log(random, 'authenticated')
-    next()
-  } else {
-    console.log(random, 'blocked by beforeEach Guard')
-    next({ name: 'pokemon-home' })
-  }
+// router.beforeEach((to, from, next) => {
+//   console.log({ to, from, next });
+//   const random = Math.random() * 100
+//   if ( random > 50 ) {
+//     console.log(random, 'authenticated')
+//     next()
+//   } else {
+//     console.log(random, 'blocked by beforeEach Guard')
+//     next({ name: 'pokemon-home' })
+//   }
+// })
+
+const canAccess = () => {
+  return new Promise( resolve => {
+    const random = Math.random() * 100
+
+    if ( random > 50 ) {
+      console.log(random, 'authenticated')
+      resolve(true)
+    } else {
+      console.log(random, 'blocked by beforeEach Guard')
+      // next({ name: 'pokemon-home' })
+      resolve(false)
+    }
+  })
+}
+
+router.beforeEach( async(to, from, next) => {
+  const authorized = await canAccess()
+
+  authorized ? next() : next({ name: 'pokemon-home' })
 })
 
 export default router;
